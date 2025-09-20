8ccdf1385573fbaeb5c54a84ec5f84eba811fb8f
nggtung2403 <nggtung2403@gmail.com>
2025-09-20 12:39:33 +0700

    souoc

--- so uoc.c
+++ so uoc.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 int main(){
-	int n;
+	long long n;
 	scanf("%d",&n);
-	int i;
-	int so_uoc = 0;
+	long long i;
+	long long so_uoc = 0;
 	for(i = 1; i <= n; i++){
 		if(n % i == 0){
 			so_uoc ++;
 		}
 	}
-	printf("%d",so_uoc);
+	printf("%lld",so_uoc);
 	return 0;
 }
